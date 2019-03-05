fd7ad274e3e1ae17f38c0ae9dc0205b78933f564
daren996 <850002098@qq.com>
2019-03-05 11:06:20 +0800

    quests ana

--- Analysis/Age.py
+++ Analysis/Age.py
@@ -12,6 +12,7 @@ rst_file_name = "rst.txt"
 age_group = {}
 age_0_6 = []
 age_60 = []
+age_7_15 = []
 with open(source_path + data_set, "r") as in_file:
     for line in in_file.readlines():
         obj = json.loads(line)
@@ -23,14 +24,19 @@ with open(source_path + data_set, "r") as in_file:
             age_0_6.append(obj["dial"][3]["content"])
         if age == "大于60岁":
             age_60.append(obj["dial"][3]["content"])
+        if age == "7~15岁":
+            age_7_15.append(obj["dial"][3]["content"])
         if age not in age_group:
             age_group[age] = 0
         age_group[age] += 1
 print(age_group)
-for c in age_0_6:
+# for c in age_0_6:
+#     print(c)
+# print("\n")
+for c in age_7_15:
     print(c)
-print("\n")
-for c in age_60:
-    print(c)
+# print("\n")
+# for c in age_60:
+#     print(c)
 
 
